f320840cf117a013054d92bfbb9064e175120836
volpino <fox91@anche.no>
2013-10-27 03:24:16 +0100

    adding autocomplete for cities

--- client/dreather.js
+++ client/dreather.js
@@ -10,28 +10,27 @@
 
   var target = 'suggest-drink';
   var targetDiv = $('#' + target);
+  var spinnerOpts = {
+    lines: 13, // The number of lines to draw
+    length: 20, // The length of each line
+    width: 10, // The line thickness
+    radius: 30, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
 
   var getDrink = function() {
     var spinner
-      , spinnerOpts = {
-          lines: 13, // The number of lines to draw
-          length: 20, // The length of each line
-          width: 10, // The line thickness
-          radius: 30, // The radius of the inner circle
-          corners: 1, // Corner roundness (0..1)
-          rotate: 0, // The rotation offset
-          direction: 1, // 1: clockwise, -1: counterclockwise
-          color: '#000', // #rgb or #rrggbb or array of colors
-          speed: 1, // Rounds per second
-          trail: 60, // Afterglow percentage
-          shadow: false, // Whether to render a shadow
-          hwaccel: false, // Whether to use hardware acceleration
-          className: 'spinner', // The CSS class to assign to the spinner
-          zIndex: 2e9, // The z-index (defaults to 2000000000)
-          top: 'auto', // Top position relative to parent in px
-          left: 'auto' // Left position relative to parent in px
-        };
-
     targetDiv.empty();
     spinner = new Spinner(spinnerOpts).spin(document.getElementById(target));
 
@@ -47,12 +46,14 @@
   };
 
   var getDrinkForPosition = function(position) {
-    window.history.pushState(
-      null, null,
-      window.location.href.split("?")[0] + "?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
-    );
+    var req_string;
 
-    $.get("gimme_drink/"+position.coords.latitude+"/"+position.coords.longitude)
+    if (position.zmw)
+      req_string = position.zmw;
+    else
+      req_string = position.coords.latitude+"/"+position.coords.longitude;
+
+    $.get("gimme_drink/"+req_string)
       .done(function (data) {
         var showDrink = function(drink_index) {
           drink_index = drink_index % data.cocktails.length;
@@ -89,6 +90,22 @@
           });
         };
 
+        $('#more-info').css('visibility', 'visible');
+        data.city = data.city || "Unknown";
+        data.weather = data.weather || "N.A.";
+        data.temperature = data.temperature || "N.A.";
+
+        $('#location-info')
+          .css('visibility', 'visible')
+          .find('input').val(data.city);
+        $('#weather-info').text(data.weather);
+        $('#temperature-info').text(data.temperature);
+
+        window.history.pushState(
+          null, null,
+          window.location.href.split("?")[0] + "?lat=" + data.lat + "&lon=" + data.lon
+        );
+
         showDrink(0);
       })
       .fail(function() {
@@ -104,8 +121,39 @@
   $(function () {
     getDrink();
 
+    $('#location-info').css('visibility', 'hidden');
+
+    $(".city-selector").autocomplete({
+      source: function(request, response) {
+        $.ajax({
+          url: "http://autocomplete.wunderground.com/aq?cb=?",
+          dataType: "jsonp",
+          data: {
+            query: request.term
+          },
+          success: function(data) {
+            response($.map(data.RESULTS, function(item) {
+              return {
+                label: item.name,
+                value: item.name,
+                zmw: item.zmw
+              }
+            }));
+          }
+        });
+      },
+      minLength: 3,
+      select: function(event, ui) {
+        var spinner;
+        targetDiv.empty();
+        $('#more-info').css('visibility', 'hidden');
+        spinner = new Spinner(spinnerOpts).spin(document.getElementById(target));
+        getDrinkForPosition({zmw: ui.item.zmw});
+      }
+    });
+
     $(document).keydown(function(e) {
-      if (e.keyCode == 13 || e.keyCode == 39) {
+      if (e.keyCode == 13) {
         $("#another-drink").click();
       }
     });
